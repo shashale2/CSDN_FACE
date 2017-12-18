2a37face4f49df55716867c5c0d87ac349569b5e
shashale2 <zhangshuaisdu2013@163.com>
2017-12-18 21:50:39 +0800

    二分查找

---
+++ src/algo/bsearch.java
@@ -0,0 +1,44 @@
+package algo;
+//¶þ·Ö²éÕÒ
+public class bsearch {
+
+	public  static void main (String [] args) throws InterruptedException
+	{
+		int [] num = {1,3,5,6,7,8,9};
+		int tar = 4;
+		
+		System.out.println(bsearch(num,tar));
+		
+	}
+	
+	public static boolean bsearch(int [] num,int tar) throws InterruptedException
+	{
+		int low =0; int  high = num.length-1; int middle = (low+high)/2;
+		while (num[middle]!=tar)
+		{
+			Thread.sleep(100);
+			int key =num[middle];
+			
+			System.out.println(key);
+			if (key>tar)
+			{
+				high = middle;
+				middle =(low+high)/2;
+				System.out.println("high"+high+"low"+low+"middle"+middle);
+			}
+			else
+			{
+				low = middle ;
+				middle =(low+high)/2;
+				System.out.println("high"+high+"low"+low+"middle"+middle);
+			}
+			if (high-low<=1&&tar!=num[low]&&tar!=num[high])
+			{
+				return false;
+			}
+				
+		}
+		return true;
+		
+	}
+}
